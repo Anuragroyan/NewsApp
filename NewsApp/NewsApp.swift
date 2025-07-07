99a3fc26436ad8d6c33a174d441660ee05d3474b
Anuragroyan <7619354965anurag@gmail.com>
2025-07-07 23:28:26 +0530

    Add initial project files

---
+++ NewsApp/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Dungeon_master on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsApp/View/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by Dungeon_master on 07/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle(" News App")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
